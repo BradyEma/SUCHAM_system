13063ad4f612763410aa6bcd67a92cc17d41d167
Niwamanya Joel <niwamanyajoel13@icloud.com>
2025-07-25 12:45:24 +0300

    Added wholesaler order viewing page

---
+++ database/migrations/2025_07_25_084906_add_sender_receiver_to_conversations_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::table('conversations', function (Blueprint $table) {
+        $table->unsignedBigInteger('sender_id')->after('id');
+        $table->unsignedBigInteger('receiver_id')->after('sender_id');
+
+        // Add foreign keys if needed
+        $table->foreign('sender_id')->references('id')->on('users')->onDelete('cascade');
+        $table->foreign('receiver_id')->references('id')->on('users')->onDelete('cascade');
+    });
+}
+
+public function down(): void
+{
+    Schema::table('conversations', function (Blueprint $table) {
+        $table->dropForeign(['sender_id']);
+        $table->dropForeign(['receiver_id']);
+        $table->dropColumn(['sender_id', 'receiver_id']);
+    });
+}
+
+};
